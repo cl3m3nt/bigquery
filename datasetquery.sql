274962902c9cd93b391bcbae0f27a2cfa7107f5d
clementlr <clement.leroux@gmail.com>
2020-02-04 14:08:47 +0100

    bigquery getstarted

---
+++ datasetquery.sql
@@ -0,0 +1,11 @@
+SELECT
+  name,
+  count
+FROM
+  `babynames.names_2014`
+WHERE
+  gender = 'M'
+ORDER BY
+  count DESC
+LIMIT
+  5
---
+++ getstarted.sql
@@ -0,0 +1,22 @@
+SELECT
+  name, gender,
+  SUM(number) AS total
+FROM
+  `bigquery-public-data.usa_names.usa_1910_2013`
+GROUP BY
+  name, gender
+ORDER BY
+  total DESC
+LIMIT
+  10
+
+
+SELECT
+  name, gender,
+ number AS total
+FROM
+  `bigquery-public-data.usa_names.usa_1910_2013`
+ORDER BY
+  total DESC
+LIMIT
+  10
